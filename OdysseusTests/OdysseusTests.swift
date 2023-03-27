5098f07d3fd27bfdb344563504a246d9be6dbd12
Matheus Alves <reservedwordbyme@gmail.com>
2023-03-27 08:57:43 -0300

    🎉 initial commit

---
+++ OdysseusTests/OdysseusTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import Odysseus
+
+final class OdysseusTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
